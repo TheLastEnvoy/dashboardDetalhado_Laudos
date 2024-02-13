919c2003771845887428567520f9102331173293
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-02-13 16:18:36 -0300

    Add files via upload

---
+++ dashboardDetalhado_laudos_v1.1.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import plotly.express as px
+
+# Carregar os dados do Excel
+file_path = "C:/laudos_SO_sharepoint_11022024.xlsx"
+df = pd.read_excel(file_path)
+
+# Definir título do aplicativo
+st.title("Análise de Laudos")
+
+# Lista de todos os técnicos, tipos de laudo e assentamentos
+tecnicos = list(df['Técnico'].unique())
+tecnicos.append("Todos")
+tipos_de_laudo = list(df['Tipo de Laudo'].unique())
+tipos_de_laudo.append("Todos")
+assentamentos = list(df['Assentamento'].unique())
+assentamentos.append("Todos")
+
+# Filtrar por técnico
+selected_tecnico = st.sidebar.selectbox("Selecione um técnico:", tecnicos)
+if selected_tecnico != "Todos":
+    filtered_df = df[df['Técnico'] == selected_tecnico]
+else:
+    filtered_df = df
+
+# Filtrar por assentamento
+selected_assentamento = st.sidebar.selectbox("Selecione um assentamento:", assentamentos)
+if selected_assentamento != "Todos":
+    filtered_df = filtered_df[filtered_df['Assentamento'] == selected_assentamento]
+
+# Filtrar por tipo de laudo
+selected_tipo_laudo = st.sidebar.selectbox("Selecione um tipo de laudo:", tipos_de_laudo)
+if selected_tipo_laudo != "Todos":
+    filtered_df = filtered_df[filtered_df['Tipo de Laudo'] == selected_tipo_laudo]
+
+# Filtrar por data
+start_date = st.sidebar.date_input("Data inicial:", min(pd.to_datetime(filtered_df['Data'], format='%d/%m/%Y')).date())
+end_date = st.sidebar.date_input("Data final:", max(pd.to_datetime(filtered_df['Data'], format='%d/%m/%Y')).date())
+filtered_df['Data'] = pd.to_datetime(filtered_df['Data'], format='%d/%m/%Y').dt.date
+filtered_df = filtered_df[(filtered_df['Data'] >= start_date) & (filtered_df['Data'] <= end_date)]
+
+# Exibir tabela interativa
+st.write(filtered_df)
+
+# Exibir gráfico interativo
+st.subheader("Contagem de Laudos por Tipo")
+chart_data = filtered_df['Tipo de Laudo'].value_counts()
+st.bar_chart(chart_data)
+
+# Gráfico de pizza
+st.subheader("Gráfico de Pizza - Distribuição dos Laudos")
+pie_chart_data = filtered_df['Tipo de Laudo'].value_counts()
+fig = px.pie(names=pie_chart_data.index, values=pie_chart_data.values, title='Distribuição dos Laudos')
+st.plotly_chart(fig)
