e249096cf89fd5c45edcb25161dd583b7f662776
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-02-13 16:29:35 -0300

    Update dashboardDetalhado_laudos_v1.1.py

--- dashboardDetalhado_laudos_v1.1.py
+++ dashboardDetalhado_laudos_v1.1.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import plotly.express as px
 
 # Carregar os dados do Excel
-file_path = "C:/laudos_SO_sharepoint_11022024.xlsx"
+file_path = "laudos_SO_sharepoint_11022024.xlsx"
 df = pd.read_excel(file_path)
 
 # Definir título do aplicativo
