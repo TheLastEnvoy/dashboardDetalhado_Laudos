7f3e73d0f01965c67735113687ab4f8d2e8185d0
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-02-15 11:14:20 -0300

    Update dashboardDetalhado_laudos_v1.1.py

--- dashboardDetalhado_laudos_v1.1.py
+++ dashboardDetalhado_laudos_v1.1.py
@@ -8,11 +8,11 @@ file_path = "laudos_SO_sharepoint_11022024.xlsx"
 df = pd.read_excel(file_path)
 
 # Definir título do aplicativo
-st.title("Relação de laudos")
+st.title("Análise de Laudos")
 
 # Lista de todos os técnicos, tipos de laudo e assentamentos
 tecnicos = ['Todos'] + list(df['Técnico'].unique())
-tipos_de_laudo = ['Todos'] + list(df['tipo de laudo'].unique())
+tipos_de_laudo = ['Todos'] + list(df['Tipo de Laudo'].unique())
 assentamentos = ['Todos'] + list(df['Assentamento'].unique())
 
 # Data inicial padrão: 01/01/2022
@@ -36,7 +36,7 @@ if selected_assentamento != "Todos":
 # Filtrar por tipo de laudo
 selected_tipo_laudo = st.sidebar.selectbox("Selecione um tipo de laudo:", tipos_de_laudo)
 if selected_tipo_laudo != "Todos":
-    filtered_df = filtered_df[filtered_df['tipo de laudo'] == selected_tipo_laudo]
+    filtered_df = filtered_df[filtered_df['Tipo de Laudo'] == selected_tipo_laudo]
 
 # Filtrar por data
 start_date = st.sidebar.date_input("Data inicial:", start_date)
@@ -48,12 +48,12 @@ filtered_df = filtered_df[(filtered_df['Data'] >= start_date) & (filtered_df['Da
 st.write(filtered_df)
 
 # Exibir gráfico interativo
-st.subheader("Gráfico de barras - tipo de laudo")
-chart_data = filtered_df['tipo de laudo'].value_counts()
+st.subheader("Contagem de Laudos por Tipo")
+chart_data = filtered_df['Tipo de Laudo'].value_counts()
 st.bar_chart(chart_data)
 
 # Gráfico de pizza
-st.subheader("Gráfico de pizza - tipo de laudo")
-pie_chart_data = filtered_df['tipo de laudo'].value_counts()
+st.subheader("Gráfico de Pizza - Distribuição dos Laudos")
+pie_chart_data = filtered_df['Tipo de Laudo'].value_counts()
 fig = px.pie(names=pie_chart_data.index, values=pie_chart_data.values, title='Distribuição dos Laudos')
 st.plotly_chart(fig)
