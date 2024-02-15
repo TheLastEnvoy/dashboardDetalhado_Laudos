3b044c55f1bef434b52135e1a9886b4f96a00207
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-02-15 11:14:55 -0300

    Update dashboardDetalhado_laudos_v1.1.py

--- dashboardDetalhado_laudos_v1.1.py
+++ dashboardDetalhado_laudos_v1.1.py
@@ -53,7 +53,7 @@ chart_data = filtered_df['Tipo de Laudo'].value_counts()
 st.bar_chart(chart_data)
 
 # Gráfico de pizza
-st.subheader("Gráfico de Pizza - Distribuição dos Laudos")
+st.subheader("Gráfico de pizza - Distribuição dos Laudos")
 pie_chart_data = filtered_df['Tipo de Laudo'].value_counts()
 fig = px.pie(names=pie_chart_data.index, values=pie_chart_data.values, title='Distribuição dos Laudos')
 st.plotly_chart(fig)
