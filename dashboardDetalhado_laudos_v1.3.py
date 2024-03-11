4f5516a4529aa74abfcedc3922ff7b06cbcf736b
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-03-11 16:18:36 -0300

    Update dashboardDetalhado_laudos_v1.3.py

--- dashboardDetalhado_laudos_v1.3.py
+++ dashboardDetalhado_laudos_v1.3.py
@@ -8,7 +8,7 @@ file_path = "laudos_SO_sharepoint_11032024.xlsx"
 df = pd.read_excel(file_path)
 
 # Definir título do aplicativo
-st.title("Indicadores SO TED INCRA/UFPR - Laudos de Supervisão Ocupacional")
+st.title("Indicadores SO - (TED INCRA/UFPR) - Laudos de Supervisão Ocupacional")
 
 st.subheader("pesquisar nomes com underline")
 
