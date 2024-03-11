1157dbf2084f65ae1efb20fc15a0cd324fc9ec71
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-03-11 15:44:41 -0300

    Update dashboardDetalhado_laudos_v1.3.py

--- dashboardDetalhado_laudos_v1.3.py
+++ dashboardDetalhado_laudos_v1.3.py
@@ -74,7 +74,7 @@ total_por_tipo_laudo.columns = ['Tipo de Laudo', 'Quantidade de Laudos']
 total_por_tipo_laudo.loc[len(total_por_tipo_laudo)] = ['Total', total_de_laudos]
 
 # Remover a coluna de índice
-total_por_tipo_laudo = total_por_tipo_laudo.drop('index', axis=1)
+total_por_tipo_laudo = total_por_tipo_laudo.drop(total_por_tipo_laudo.columns[0], axis=1)
 
 # Exibir quadro com os totais
 st.subheader("Total de Laudos por Tipo de Laudo")
